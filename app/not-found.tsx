0b6e820e36fb3b5963617e5c2ac9c51fd06727c0
Crazy_Fox <holmyliszag@ukr.net>
2026-02-08 17:41:24 +0200

    return metadata

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,23 +6,6 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { Metadata } from 'next';
 
-export default function NotFound() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const timer = setTimeout(() => router.push('/'), 3000);
-    return () => clearTimeout(timer);
-  }, [router]);
-  return (
-    <div className={css.main}>
-      <h1 className={css.title}>404 - Page not found</h1>
-      <p className={css.description}>
-        Sorry, the page you are looking for does not exist.
-      </p>
-    </div>
-  );
-}
-
 export const metadata: Metadata = {
   title: 'Not found',
   description: 'Page is not found',
@@ -40,3 +23,20 @@ export const metadata: Metadata = {
     ],
   },
 };
+
+export default function NotFound() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => router.push('/'), 3000);
+    return () => clearTimeout(timer);
+  }, [router]);
+  return (
+    <div className={css.main}>
+      <h1 className={css.title}>404 - Page not found</h1>
+      <p className={css.description}>
+        Sorry, the page you are looking for does not exist.
+      </p>
+    </div>
+  );
+}
